a04513262f7b02969d16fc784eb9a3451bfce65c
DoolotbekovSultan <dsn14.11.2008@gmail.com>
2026-04-21 16:13:29 +0600

    add base remote datasource

--- lib/packages/nexo_core/datasources/remote/base_remote_datasources.dart
+++ lib/packages/nexo_core/datasources/remote/base_remote_datasources.dart
@@ -1,1 +1,190 @@
-class BaseRemoteDatasources {}
+import 'package:dio/dio.dart';
+import 'package:nexo/packages/nexo_core/network/dio_client.dart';
+import 'package:nexo/packages/nexo_logger/nexo_logger.dart';
+
+
+abstract class BaseRemoteDataSource {
+  final DioClient client;
+  final NexoLogger _logger;
+
+  const BaseRemoteDataSource(
+    this.client, {
+    required NexoLogger logger,
+  }) : _logger = logger;
+
+  String _tag(String message) => '[Remote] $message';
+
+  Future<R> _run<R>(
+    String action,
+    Future<R> Function() operation,
+  ) async {
+    try {
+      return await operation();
+    } on DioException catch (error, stackTrace) {
+      _logger.error(
+        message: _tag(action),
+        error: error,
+        stackTrace: stackTrace,
+      );
+      rethrow;
+    } catch (error, stackTrace) {
+      _logger.error(
+        message: _tag(action),
+        error: error,
+        stackTrace: stackTrace,
+      );
+      rethrow;
+    }
+  }
+
+  Future<Response<T>> get<T>(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress,
+  }) {
+    return _run(
+      'GET failed path=$path',
+      () => client.get<T>(
+        path,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+      ),
+    );
+  }
+
+  Future<Response<T>> post<T>(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) {
+    return _run(
+      'POST failed path=$path',
+      () => client.post<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      ),
+    );
+  }
+
+  Future<Response<T>> put<T>(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) {
+    return _run(
+      'PUT failed path=$path',
+      () => client.put<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      ),
+    );
+  }
+
+  Future<Response<T>> patch<T>(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) {
+    return _run(
+      'PATCH failed path=$path',
+      () => client.patch<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      ),
+    );
+  }
+
+  Future<Response<T>> delete<T>(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+  }) {
+    return _run(
+      'DELETE failed path=$path',
+      () => client.delete<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+      ),
+    );
+  }
+
+  Future<Response> download(
+    String urlPath,
+    String savePath, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress,
+  }) {
+    return _run(
+      'DOWNLOAD failed url=$urlPath',
+      () => client.download(
+        urlPath,
+        savePath,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+      ),
+    );
+  }
+
+  Future<Response<T>> postFormData<T>(
+    String path, {
+    required FormData data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) {
+    return _run(
+      'POST FORM-DATA failed path=$path',
+      () => client.postFormData<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      ),
+    );
+  }
+}
